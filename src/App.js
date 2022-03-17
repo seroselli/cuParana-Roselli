22246f95c33236a87faead34fb94aab534e040d9
Sebastian Roselli <sebasroselli@hotmail.com>
2022-03-16 22:31:34 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+        <div className="parallax">
+        </div>
+        <div className="titulo">Prueba mi app de React</div>
+        <div className="parallax">
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
